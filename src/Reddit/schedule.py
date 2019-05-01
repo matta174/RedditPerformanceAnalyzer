e3fc31953285b41249d69e37945fa0b4868c862b
Matthew Compton <matthewc174@gmail.com>
2019-05-01 19:57:35 -0400

    scheduler now can run data collection
    Signed-off-by: Matthew Compton <matthewc174@gmail.com>

--- src/Reddit/schedule.py
+++ src/Reddit/schedule.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+from Reddit import data_collection
 class scheduler:
 
     def insert_job_in_queue(self,id,batchId,submissionId,scheduledTime,jobType='collect25new',subreddit='all'):
@@ -15,28 +15,6 @@ class scheduler:
                 print(e)
 
 
-	# Status codes:
-	# 0 = In Queue
-	# 1 = In progress
-	# 2 = Complete
-	# 3 = Error / Misc
-    def check_queue(self,test =  'test'):
-        try:
-                conn = sqlite3.connect('src\\Data\\submissions.db')
-                sql = ''' SELECT * FROM process_queue ORDER BY ROWID ASC LIMIT 1'''
-                cur = conn.cursor()
-                cur.execute(sql)
-                conn.commit()
-                rows = cur.fetchall()
-                final_result = [list(i) for  i in rows]
-                #code for after check complete logic
-                #switch for job type
-
-
-        except Exception as e:
-                print(e)
-
-    
     def update_queue(self, id, status):
         try:
                 conn = sqlite3.connect('src\\Data\\submissions.db')
@@ -47,3 +25,34 @@ class scheduler:
                 return cur.lastrowid
         except Exception as e:
             print(e)
+
+	# Status codes:
+	# 0 = In Queue
+	# 1 = In progress
+	# 2 = Complete
+	# 3 = Error / Misc
+    def check_queue(self,test =  'test'):
+        try:
+                conn = sqlite3.connect('src\\Data\\submissions.db')
+                sql = '''  SELECT * FROM process_queue where status = '0' ORDER BY ROWID ASC LIMIT 1 '''
+                cur = conn.cursor()
+                cur.execute(sql)
+                conn.commit()
+                rows = cur.fetchall()
+                final_result = [list(i) for  i in rows]
+
+                for value in final_result:
+                        if (value[1] == 'Collect'): 
+                                data_collection.collect_submission_ids(value[4], value[3],value[2])
+                                self.update_queue(value[0],'2')
+
+                        else:
+                                self.update_queue(value[0],'3')
+                
+
+
+        except Exception as e:
+                print(e)
+
+    
+
--- src/main.py
+++ src/main.py
@@ -8,8 +8,9 @@ from Reddit import data_collection, submission_data, database_interactions, sche
 def main():
     x = True
     while x:
+        s = schedule.scheduler()
         p = Process(target= data_collection.collect_submission_ids())
-        p2 = Process(target = check_queue.check_queue())
+        p2 = Process(target = s.check_queue())
         p2.start()
         p2.join()
         p.start()

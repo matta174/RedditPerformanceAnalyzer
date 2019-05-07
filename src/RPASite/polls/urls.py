b3bf0f9e788e67beb98307e82ea0a0abfbe927f5
Matthew Compton <matthewc174@gmail.com>
2019-05-07 16:06:39 -0400

    quick update to launch.json
    Signed-off-by: Matthew Compton <matthewc174@gmail.com>

--- src/RPASite/polls/urls.py
+++ src/RPASite/polls/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('/charts/',src.Reddit.data_visualization.create_bar_chart())
+    path('<int:question_id>/vote/', views.vote, name='vote')
+    #path('/charts/',src.Reddit.data_visualization.create_bar_chart()) 
 ]

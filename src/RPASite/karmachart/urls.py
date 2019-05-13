c3f2d06718aa44a77829929327fa6ed0ed716450
Matthew Compton <matthewc174@gmail.com>
2019-05-13 16:17:52 -0400

    finally cracked using django for our needs
    Signed-off-by: Matthew Compton <matthewc174@gmail.com>

--- src/RPASite/karmachart/urls.py
+++ src/RPASite/karmachart/urls.py
@@ -1,10 +1,10 @@
 import sys, os
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 app_name = 'karmachart'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('',views.IndexView.collect_data,name='collect_data'),
-    path('clear_session', views.IndexView.clear_sesh, name='clear_sesh')
+    url(r'^collect_data',views.collect_data,name='collect_data')
 ]
--- src/RPASite/karmachart/views.py
+++ src/RPASite/karmachart/views.py
@@ -1,24 +1,23 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+import requests
 from django.views import generic
 from .models import Job
 import Reddit.data_collection
 
+def collect_data(request):
+    Reddit.data_collection.collect_submission_ids('flying', 'top',5)
+    return render(request,'karmachart/index.html')
 
 class IndexView(generic.ListView):
     template_name = 'karmachart/index.html'
-    context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         """Return the last five published questions."""
         return 'testing'
 
-    def collect_data(self):
-        Reddit.data_collection.collect_submission_ids('flying', 'top',5)
-        return 'blank'
-    # def add_job(self):
-    def clear_sesh(self,request):
-        print('SESSION CLEARED')
-        return 'test'
+
+
+
 
